5fd07825955dc96cca571be6db5752370bc45e21
Chiranjiv <chiranjiv9022@gmail.com>
2023-04-12 09:03:52 +0530

    Modified cockpit component with Local Reference Example

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -16,10 +16,18 @@ export class CockpitComponent implements OnInit {
   constructor(){}
   ngOnInit(){}
 
-  onAddServer(){
-  // EventEmit :- https://www.udemy.com/course/the-complete-guide-to-angular-2/learn/lecture/6656076#announcement
-    this.serverCreated.emit({serverName: this.newServerName, serverContent: this.newServerContent})
-  }
+  // // Using ngModel
+  // onAddServer(){
+  // // EventEmit :- https://www.udemy.com/course/the-complete-guide-to-angular-2/learn/lecture/6656076#announcement
+  //   this.serverCreated.emit({serverName: this.newServerName, serverContent: this.newServerContent})
+  // }
+  
+  // Using Local Reference:- https://www.udemy.com/course/the-complete-guide-to-angular-2/learn/lecture/6656092#overview
+  onAddServer(LocalReferenceName: HTMLInputElement){
+    // EventEmit :- https://www.udemy.com/course/the-complete-guide-to-angular-2/learn/lecture/6656076#announcement
+      console.log("Local Reference Value is the Whole Input:-", LocalReferenceName)
+      this.serverCreated.emit({serverName: this.newServerName, serverContent: this.newServerContent})
+    }
   onAddBlueprint(){
   // EventEmit :- https://www.udemy.com/course/the-complete-guide-to-angular-2/learn/lecture/6656076#announcements
     this.blueprintCreated.emit({serverName: this.newServerName, serverContent: this.newServerContent})
